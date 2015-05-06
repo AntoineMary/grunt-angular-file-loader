0fcf099c8cc5910aef0295712dc61021a7678ce6
Antoine Mary <antoinee.mary@gmail.com>
2015-05-07 01:37:38 +0200

    New tests

--- test/angular_filesort_test.js
+++ test/angular_filesort_test.js
@@ -22,29 +22,93 @@ var grunt = require('grunt');
     test.ifError(value)
 */
 
-exports.angular_file_loader = {
+exports.angularFileLoader = {
   setUp: function(done) {
     // setup here if necessary
     done();
   },
 
-  default_options: function(test) {
+  custom_tag_html: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('test/fixtures/default_options.html');
-    var expected = grunt.file.read('test/expected/default_options.html');
+    var actual = grunt.file.read('test/fixtures/web/html/custom_tag.html');
+    var expected = grunt.file.read('test/expected/html/custom_tag.html');
+    test.equal(actual, expected, 'Injection must be done');
+
+    test.done();
+  },
+
+  default_options_html: function(test) {
+    test.expect(1);
+
+    var actual = grunt.file.read('test/fixtures/web/html/default_options.html');
+    var expected = grunt.file.read('test/expected/html/default_options.html');
 
     test.equal(actual, expected, 'Injection must be done');
 
     test.done();
   },
 
-  custom_options: function(test) {
+  relative_false_html: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('test/fixtures/custom_options.html');
-    var expected = grunt.file.read('test/expected/custom_options.html');
+    var actual = grunt.file.read('test/fixtures/web/html/relative_false.html');
+    var expected = grunt.file.read('test/expected/html/relative_false.html');
+
     test.equal(actual, expected, 'Injection must be done');
+
+    test.done();
+  },
+
+  relative_string_html: function(test) {
+    test.expect(1);
+
+    var actual = grunt.file.read('test/fixtures/web/html/relative_string.html');
+    var expected = grunt.file.read('test/expected/html/relative_string.html');
+
+    test.equal(actual, expected, 'Injection must be done');
+
+    test.done();
+  },
+  custom_tag_jade: function(test) {
+    test.expect(1);
+
+    var actual = grunt.file.read('test/fixtures/web/jade/custom_tag.jade');
+    var expected = grunt.file.read('test/expected/jade/custom_tag.jade');
+    test.notEqual(actual, expected, 'Injection must be done but indent is not correct');
+
+    test.done();
+  },
+
+  default_options_jade: function(test) {
+    test.expect(1);
+
+    var actual = grunt.file.read('test/fixtures/web/jade/default_options.jade');
+    var expected = grunt.file.read('test/expected/jade/default_options.jade');
+
+    test.notEqual(actual, expected, 'Injection must be done but indent is not correct');
+
+    test.done();
+  },
+
+  relative_false_jade: function(test) {
+    test.expect(1);
+
+    var actual = grunt.file.read('test/fixtures/web/jade/relative_false.jade');
+    var expected = grunt.file.read('test/expected/jade/relative_false.jade');
+
+    test.notEqual(actual, expected, 'Injection must be done but indent is not correct');
+
+    test.done();
+  },
+
+  relative_string_jade: function(test) {
+    test.expect(1);
+
+    var actual = grunt.file.read('test/fixtures/web/jade/relative_string.jade');
+    var expected = grunt.file.read('test/expected/jade/relative_string.jade');
+
+    test.notEqual(actual, expected, 'Injection must be done but indent is not correct');
 
     test.done();
   }
